80c12c31957124cd49971dd4f43e2c8b3e30abef
Melophylic <mnadzimtahara@gmail.com>
2025-05-09 10:55:56 +0700

    add createsuperuser using migration

--- build_files.sh
+++ build_files.sh
@@ -1,7 +1,4 @@
 env\Scripts\activate
 pip install -r requirements.txt
 python3.9 manage.py collectstatic
-python manage.py createsuperuser
-thariz
-davianoctavio@gmail.com
-admin123
+# python3.9 manage.py create_superuser
